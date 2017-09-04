2c15ea5938195b9d4215c65835bdf9a35fb7d87a
Maheswariviji <maheswarivijaya1@gmail.com>
2017-09-04 16:26:22 +0530

    login

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -2,10 +2,13 @@ import { RouterModule, Routes } from '@angular/router';
 import{NgModule} from  '@angular/core';
 import{LoginComponent} from './components/login/login.component';
 import{RegisterComponent} from './components/register/register.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+
 const appRoutes: Routes = [
                           
                            { path: 'login', component: LoginComponent },
                            { path: 'register', component: RegisterComponent },
+                            { path: 'dashboard', component: DashboardComponent }
                       ];
 @NgModule({
     declarations: [],
