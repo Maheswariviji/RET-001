48985224a78d671d462743f9b01a4f5e15f9176b
Dhiva04 <dhivakaran.ret@gmail.com>
2017-09-04 17:55:50 +0530

    register merged

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -10,7 +10,6 @@ const appRoutes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'forgot', component: ForgotPasswordComponent },
-
     { path: 'dashboard', component: DashboardComponent }
 ];
 @NgModule( {
